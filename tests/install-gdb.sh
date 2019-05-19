925a4ba248051320fb76a9adf3e02c79aa7a1c27
Unknown <mbalabin@gmail.com>
2019-05-19 21:49:32 +0700

    Added gdb-python2.7 package to repository

--- tests/install-gdb.sh
+++ tests/install-gdb.sh
@@ -5,9 +5,7 @@ sudo apt-get update -qq
 
 if [[ "$1" ==  yes ]]
 then
-    # https://packages.debian.org/jessie-backports/gdb-python2
-    curl -o gdb-python2.deb 'http://http.us.debian.org/debian/pool/main/g/gdb/gdb-python2_7.11.1-2~bpo8+1_amd64.deb'
-    sudo dpkg --install --force-depends gdb-python2.deb
+    sudo dpkg --install --force-depends "$TRAVIS_BUILD_DIR/tests/gdb-python2_7.11.1-2_bpo8+1_amd64.deb"
     sudo apt-get -f -qq install
 else
     sudo apt-get -qq install gdb
